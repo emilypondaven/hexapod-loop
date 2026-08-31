efcac7f47c5864f2fbcb6fe28f4591b7919ff142
emilypondaven <emily.pondaven@gmail.com>
2026-08-31 17:59:54 +0100

    Changes to folder structure

---
+++ analyse.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+
+batched_configs = {}
+logs_df = pd.read_csv("logs/trials.csv")
+
+# Get number of batches
+for 
+with open("config.json") as f:
+    config = json.load(f)
+
+
+## Look through batches from trials.csv
+## Separate by batches
+## Turn each batch into a summary
--- relax.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('/home/emily/Freenove_Big_Hexapod_Robot_Kit_for_Raspberry_Pi/Code/Server')
-
-from control import Control
-
-control = Control()
-control.relax(True)
